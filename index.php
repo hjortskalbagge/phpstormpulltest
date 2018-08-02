c504098e6f9a035cbfe1deff0ee0af3482781d75
Fiete Korb <fiete.korb@deinhandy.de>
2018-08-02 12:58:31 +0200

    4

--- index.php
+++ index.php
@@ -4,3 +4,5 @@ echo ('first change made directly from within github');
 echo ('second change made directly from within github');
 
 echo ('third change made directly from within github');
+
+echo ('fourth change made directly from within github');
